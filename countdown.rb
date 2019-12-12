fcf21f336aa7bd8dd20d1f6a6d3cc2eafba778bc
Arthur White <mister.arthur.white@gmail.com>
2019-12-12 04:14:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    countdown_with_sleep(1)
+    count -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  
+end
+
+def countdown_with_sleep (seconds)
+  sleep (seconds)
 end
