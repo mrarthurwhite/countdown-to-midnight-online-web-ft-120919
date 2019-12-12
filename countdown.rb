c5e3382c5d070fe4964cdfdffc6ccac18aa6040e
Arthur White <mister.arthur.white@gmail.com>
2019-12-12 04:13:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,13 @@
 def countdown(count)
   while count > 0
     puts "#{count} SECOND(S)!"
-    sleep (0.5)
+    
     count -= 1
   end
   return "HAPPY NEW YEAR!"
   
+end
+
+def countdown_with_sleep (seconds)
+  sleep (seconds)
 end
